841e8aa3dc97e12777a8b1f6f94e573b2ba9967f
ABEL CARVAJAL <joseabel.carvajal@gmail.com>
2022-08-17 16:00:45 -0500

    Encriptador, proyecto casi terminado

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Documentación metodo "replace" https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/replace */
+
+function tocript(){
+    var texto = document.getElementById("texto").value.toLowerCase();
+    var textoCript = texto.replace(/e/img, "enter");
+    var textoCript = textoCript.replace(/i/img, "imes");
+    var textoCript = textoCript.replace(/a/img, "ai");
+    var textoCript = textoCript.replace(/o/img, "ober");
+    var textoCript = textoCript.replace(/u/img, "ufat");
+    document.getElementById("textdecrypt").innerHTML = textoCript;
+}
+
+function todecript(){
+    var texto = document.getElementById("texto").value.toLowerCase();
+    var textoCript = texto.replace(/enter/img, "e");
+    var textoCript = textoCript.replace(/imes/img, "i");
+    var textoCript = textoCript.replace(/ai/img, "a");
+    var textoCript = textoCript.replace(/ober/img, "o");
+    var textoCript = textoCript.replace(/ufat/img, "u");
+    document.getElementById("textdecrypt").innerHTML = textoCript;
+}
+
+/*Documentacion Copiar texto https://www.delftstack.com/es/howto/javascript/javascript-copy-to-clipboard/*/
+
+function copiar(){
+    var copiartexto = document.getElementById("textdecrypt");
+    copiartexto.select();
+    document.execCommand('copy');
+    alert("Copiado!")
+
+}
